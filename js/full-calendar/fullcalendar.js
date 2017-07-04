c415886caecabd3a8da666c9822e513b58bfb79e
Abarajithan <abarajithan.ramachandran@mobinius.com>
2017-07-04 18:12:55 +0530

    SA pane

--- js/full-calendar/fullcalendar.js
+++ js/full-calendar/fullcalendar.js
@@ -3187,7 +3187,7 @@ function ResourceView(element, calendar, viewName) {
         }
 		
         slotScroller =
-        $("<div style='position:absolute;width:100%;overflow-x:hidden;overflow-y:auto'/>")
+        $("<div id='scrollarea' style='position:absolute;width:100%;overflow-x:hidden;overflow-y:auto'/>")
         .appendTo(slotLayer);
 				
         slotContent =
